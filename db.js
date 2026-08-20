5db64e54e2cda3ae7f06625be6928f99a52451f1
visionschoolteacher-Admin <visionschoolteacher@gmail.com>
2026-08-20 21:38:21 +0700

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@
 // ============================================
 
 const DB_NAME = "personalGradingSystemDB";
-const DB_VERSION = 5;
+const DB_VERSION = 6;
 
 const STORES = {
   students: "students",
